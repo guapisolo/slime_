aacb1110da924cb3d087b354c1ee322f0f262920
Jiajun Li <guapisolo@gmail.com>
2025-08-01 02:38:04 -0700

    add mimo model config and update converting hf to torch dict

---
+++ scripts/models/mimo-7B-rl.sh
@@ -0,0 +1,24 @@
+MODEL_ARGS=(
+    --swiglu
+    --num-layers 36
+    --hidden-size 4096
+    --ffn-hidden-size 11008
+    --num-attention-heads 32
+    --group-query-attention
+    --num-query-groups 8
+    --use-rotary-position-embeddings
+    --disable-bias-linear
+    --add-qkv-bias
+    --normalization "RMSNorm"
+    --norm-epsilon 1e-5
+    --rotary-base 640000
+    --vocab-size 151680
+    --max-position-embeddings 32768
+    --seq-length 32768
+    --untie-embeddings-and-output-weights
+    --post-self-attn-layernorm
+    --post-mlp-layernorm
+    --rotary-interleaved
+    --rotary-percent 0.5
+    --no-rope-fusion
+)
